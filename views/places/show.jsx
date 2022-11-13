1c6f6c5a5dd2dac817d18c387b465f14395bab94
Carmen Villalobos <carmenvillalobos@Carmens-MBP.attlocal.net>
2022-11-13 13:11:53 -0800

    Made a helper method on the place schema

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,11 +9,19 @@ function show (data) {
             <div>
               <img src={data.place.pic} alt={data.place.name} />
             </div>
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
             <div>
               <h2>Rating</h2>
               <p>Not Rated</p>
               <h2>Description</h2>
-              <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines} </p>
+              <h3>
+                {data.place.showEstablished()}
+              </h3>
+              <h4>
+                Serving {data.place.cuisines}
+              </h4>
               <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
               </a>      
